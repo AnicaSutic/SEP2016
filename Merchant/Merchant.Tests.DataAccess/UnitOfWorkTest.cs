8965622b6e1543dc8544dc8cc2a3ded776a93b31
Nenad <nenad.rad933@gmail.com>
2016-11-30 23:41:34 +0100

    Added Data Access Layer for Merchant (generic repository & unit of work).

---
+++ Merchant/Merchant.Tests.DataAccess/UnitOfWorkTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Merchant.DataAccess;
+using Merchant.DataAccess.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Merchant.Tests.DataAccess
+{
+    [TestClass]
+    public class UnitOfWorkTest
+    {
+        [TestMethod]
+        public void AddNewBuyerTest()
+        {
+            var unitOfWork = new UnitOfWork();
+            bool insertSuccessful;
+            var newBuyer = new Buyer
+            {
+                Name = "Testname",
+                Surname = "Testsurname"
+            };
+            try
+            {
+                unitOfWork.BuyerRepository.Insert(newBuyer);
+                unitOfWork.Save();
+                insertSuccessful = true;
+            }
+            catch(Exception ex)
+            {
+                insertSuccessful = false;
+                Console.WriteLine(ex.Message);
+            }
+
+            Assert.AreEqual(true, insertSuccessful);
+        }
+
+        [TestMethod]
+        public void GetBuyersTest()
+        {
+            var unitOfWork = new UnitOfWork();
+            var buyers = unitOfWork.BuyerRepository.Get();
+            Assert.AreNotEqual(0, buyers.Count());
+        }
+    }
+}
