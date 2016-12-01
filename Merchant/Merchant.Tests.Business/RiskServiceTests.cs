85d4582960da4308c4964a3f2ffbc7b5013660c2
Nenad <nenad.rad933@gmail.com>
2016-12-01 23:14:08 +0100

    Created business layer & added risk service.

---
+++ Merchant/Merchant.Tests.Business/RiskServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Merchant.Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Merchant.Tests.Business
+{
+    [TestClass]
+    public class RiskServiceTests
+    {
+        [TestMethod]
+        public void GetRisksByCategoryTest()
+        {
+            var service = new RiskService();
+            var risks = service.GetRiskByCategory(1);
+
+            Assert.AreEqual(2, risks.Count());
+        }
+    }
+}
