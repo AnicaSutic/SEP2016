4bdc1b90f7ceb2cc20da2cbbdaf273dd7eee566a
anica <anica.sutic@gmail.com>
2016-12-19 23:46:24 +0100

    Localize navbar,and calculator form.

---
+++ Merchant/Merchant.Web/scripts/controllers/navbarController.js
@@ -0,0 +1,7 @@
+﻿app.controller('NavbarController', function ($scope, $state, $translate) {
+
+    $scope.changeCurrentLanguage = function (key) {
+        $translate.use(key);
+    };
+
+});
--- Merchant/Merchant.Web/scripts/modules/app.js
+++ Merchant/Merchant.Web/scripts/modules/app.js
@@ -1,6 +1,6 @@
-﻿var app = angular.module('app', ['ui.router']);
+﻿var app = angular.module('app', ['ui.router', 'ngSanitize', 'pascalprecht.translate']);
 
-app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $translateProvider) {
 
     $locationProvider.hashPrefix('!').html5Mode(true);
 
@@ -23,4 +23,45 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             controller: 'HomeController'
         });
 
+    var eng =
+        {
+            english: "English",
+            serbian: "Serbian",
+            home: "Home",
+            insurance: "Insurance package",
+            calculate: "Calculate the price",
+            duration: "Insurance duration",
+            region: "Region",
+            number: "Number of insurants",
+            age: "Age",
+            sport: "Sport",
+            value: "Insured value",
+            calc: "Calculate",
+            total: "Total price",
+            calculatePrice: "Calculate the price of your insurance.",
+            travelInsurance: "Travel insurance"
+        };
+    var ser =
+        {
+            english: "Engleski",
+            serbian: "Srpski",
+            home: "Početna strana",
+            insurance: "Osiguranje",
+            calculate: "Izračunajte cenu osiguranja",
+            duration: "Trajanje osiguranja",
+            region: "Region",
+            number: "Broj osiguranika",
+            age: "Starost",
+            sport: "Sport",
+            value: "Osiguravajuća vrednost",
+            calc: "Izračunaj",
+            total: "Ukupna cena",
+            calculatePrice: "Izračunajte cenu vašeg osiguranja",
+            travelInsurance: "Putno osiguranje"
+
+        };
+
+    $translateProvider.translations('en', eng);
+    $translateProvider.translations('sr', ser);
+    $translateProvider.preferredLanguage('en');
 });
