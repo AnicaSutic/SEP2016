9b17625c5923f00b7bc6a4bce2204e679c1ec42a
anica <anica.sutic@gmail.com>
2017-01-23 17:44:23 +0100

    Adding package json file and gulp file with default task. Working.

---
+++ Merchant/Merchant.Web/gulpfile.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('default', function () {
+    // place code for your default task here
+    console.log("Merchant Application");
+});
