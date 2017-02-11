cd7afc43eb3cd001ef5a792cb30def790f40e340
Nenad <nenad.rad933@gmail.com>
2017-02-11 18:51:03 +0100

    Payment url generated & redirection after successful authentication.

---
+++ PCC/PCC.Business/PaypalService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using PayPal;
+using PayPal.Api;
+using PayPal.Log;
+
+namespace PCC.Business
+{
+    public class PaypalService
+    {
+        public string CreatePayment(string orderId, double price)
+        {
+            var apiContext = PaypalConfiguration.GetApiContext();
+
+            var details = new Details
+            {
+                shipping = "0",
+                subtotal = price.ToString(CultureInfo.InvariantCulture),
+                tax = "0",
+            };
+
+            var amount = new Amount
+            {
+                currency = "USD",
+                total = price.ToString(CultureInfo.InvariantCulture),
+                details = details,
+            };
+
+            var transaction = new Transaction
+            {
+                amount = amount,
+            };
+
+            var transactions = new List<Transaction> { transaction };
+
+
+            var payer = new Payer
+            {
+                payment_method = "paypal"
+            };
+
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = "https://localhost:44398/index.html",
+                return_url = "https://localhost:44398/index.html"
+            };
+
+            var paymet = new Payment
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactions,
+                redirect_urls = redirUrls
+            };
+
+            try
+            {
+                var createdPayment = paymet.Create(apiContext);
+                var links = createdPayment.links.GetEnumerator();
+                while (links.MoveNext())
+                {
+                    var link = links.Current;
+                    if (link.rel.ToLower().Trim().Equals("approval_url"))
+                    {
+                        return link.href;
+                    }
+                }
+            }
+            catch (PayPalException ex)
+            {
+                Debug.WriteLine("Error while creating payment url: " + ex.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ PCC/PCC/Models/PaymentRequestDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PCC.Models
+{
+    public class PaymentRequestDetails
+    {
+        public string OrderId { get; set; }
+        public double Price { get; set; }
+    }
+}
